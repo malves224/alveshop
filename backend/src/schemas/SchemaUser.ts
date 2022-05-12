4c08388e34a916066ac38748f05ac68597461c6a
Matheus Alves <malves224@gmail.com>
2022-05-12 14:17:20 -0300

    feat (schema/User): add validação schema para "user".

---
+++ backend/src/schemas/SchemaUser.ts
@@ -0,0 +1,34 @@
+import Joi from 'joi';
+
+class UserSchema {
+  typeMessage = {
+    name: {
+      'any.required': 'Nome é obrigatório.',
+      'string.base': 'Nome com tipo de dado errado.',
+      'string.min': 'Nome deve ser maior que 2 caracteres.',
+      'string.empty': 'Nome é obrigatório.',
+    },
+    email: {
+      'any.required': 'Email é obrigatório.',
+      'string.base': 'Email com tipo de dado errado.',
+      'string.pattern.base': 'Email com formato invalido.',
+      'string.empty': 'Email é obrigatório.',
+    },
+    password: {
+      'any.required': 'Senha é obrigatório.',
+      'string.base': 'Senha com tipo de dado errado.',
+      'string.min': 'Senha deve ser maior que 6 caracteres.',
+      'string.empty': 'Senha é obrigatório.',
+    },
+  };
+
+  schema = Joi.object({
+    name: Joi.string().min(3).required().messages(this.typeMessage.name),
+    email: Joi.string().pattern(/\S+@\S+\.\S+/)
+      .required().messages(this.typeMessage.email),
+    password: Joi.string().min(6).required()
+      .messages(this.typeMessage.password),
+  });
+}
+
+export default UserSchema;
