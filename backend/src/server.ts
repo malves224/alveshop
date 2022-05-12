62febc12c6dc4eb7f793806ba83b19b39daa4091
Matheus Alves <malves224@gmail.com>
2022-05-12 17:33:11 -0300

    feat (ProductRouter): add rotas GET,PUT,POST, e DELETE para product.
    Aplica os middlewares de verificações como validationsSchema, CheckUserToken e CheckIfUserAdmin, a ordem dos middleware é importante.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import App from './app';
+import productRouter from './routes/ProductRouter';
 import userRouter from './routes/UserRouters';
 
 const server = new App();
 
 server.addRouter(userRouter);
+server.addRouter(productRouter);
 
 export default server;
