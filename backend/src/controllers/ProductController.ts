6f488ec356edde92d7af24bda94a768fee9982d4
Matheus Alves <malves224@gmail.com>
2022-05-12 19:31:28 -0300

    feat (ProductController): add controller findAll com termos de pesquisa.

--- backend/src/controllers/ProductController.ts
+++ backend/src/controllers/ProductController.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from 'express';
 import Products from '../database/models/products';
 import { IProduct } from '../interfaces/product';
 import ProductSchema from '../schemas/SchemaProduct';
@@ -11,6 +12,18 @@ class ProductController extends GenericController<IProduct, Products> {
     schema = new ProductSchema().schema,
   ) {
     super(route, service, schema);
+  }
+
+  async findAll(req: Request, res: Response) {
+    const { name, priceGreaterThan, priceLessThan, active } = req.query;
+    const list = await this.service
+      .findAll({ 
+        name: name as string, 
+        priceGreaterThan: priceGreaterThan as string, 
+        priceLessThan: priceLessThan as string,
+        active: active as string,
+      });
+    return res.status(200).json(list);
   }
 }
 
