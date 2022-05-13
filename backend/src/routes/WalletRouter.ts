a56895930a240a516c09c3a0fb8367540d85b85f
Matheus Alves <malves224@gmail.com>
2022-05-13 12:46:28 -0300

    feat (WalletRouter): Add rotas com metodo GET e PUT.
    A carteira é criada no momento que se cria um usuario cliente.

---
+++ backend/src/routes/WalletRouter.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import AuthorizationController from '../controllers/AuthorizationController';
+import WalletController from '../controllers/WalletController';
+
+const walletController = new WalletController('/wallet');
+const authorizationController = new AuthorizationController();
+const { route, validationsSchema } = walletController;
+const routeId = `${route}/:id`;
+const walletRouter = Router();
+
+walletRouter
+  .get(
+    routeId,
+    authorizationController.checkUserToken,
+    AuthorizationController.checkIfUserAdmin,  
+    (req, res) => walletController.findOne(req, res),
+  )
+  .get(
+    route, 
+    authorizationController.checkUserToken,
+    AuthorizationController.checkAuthorshipObject,
+    (req, res) => walletController.findAll(req, res),
+  )
+  .put(
+    routeId,
+    validationsSchema,
+    authorizationController.checkUserToken,
+    AuthorizationController.checkIfUserAdmin,
+    (req, res) => walletController.update(req, res),
+  );
+
+export default walletRouter;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,14 @@
 import App from './app';
 import productRouter from './routes/ProductRouter';
 import userRouter from './routes/UserRouters';
+import walletRouter from './routes/WalletRouter';
 
 const server = new App();
 
-server.addRouter(userRouter);
-server.addRouter(productRouter);
+server.addRouter([
+  userRouter,
+  productRouter,
+  walletRouter,
+]);
 
 export default server;
