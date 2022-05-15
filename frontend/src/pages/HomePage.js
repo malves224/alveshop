ef2fd39e2c94bbb292e8c2e9fb21496827c21b27
Matheus Alves <malves224@gmail.com>
2022-05-15 18:48:48 -0300

    feat (HomePage): add renderização dos produtos utilizando productCard.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import { Container } from '@mui/material';
+import React, { useContext } from 'react';
+import Navbar from '../components/Navbar';
+import productsDataContext from '../context/Context';
+import ProductCard from '../components/ProductCard';
+import SearchBar from '../components/SearchBar';
 
 function HomePage() {
+  const { products } = useContext(productsDataContext);
+
   return (
-    <div>HomePage</div>
+    <>
+      <Navbar />
+      <SearchBar />
+      <Container
+        sx={ {
+          mt: 2,
+          display: 'flex',
+          flexFlow: 'wrap row',
+        } }
+      >
+        {
+          products && products.map((product) => (
+            <ProductCard itemProduct={ product } key={ product.id } />
+          ))
+        }
+      </Container>
+    </>
   );
 }
 
