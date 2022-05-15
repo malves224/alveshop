d11e29e4dc9c79595daa7990330e581659e09093
Matheus Alves <malves224@gmail.com>
2022-05-15 19:26:21 -0300

    feat (App) add alertTogle no app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
 import { Login, HomePage } from './pages';
+import AlertTogle from './components/AlertTogle';
+import productsDataContext from './context/Context';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
+  const { alertGlobal, setOpenAlert } = useContext(productsDataContext);
+  const { open, severity } = alertGlobal;
+
   return (
-    <Switch>
-      <Route
-        exact
-        path="/login"
-        render={ () => <Login /> }
-      />
-      <Route
-        exact
-        path="/"
-        render={ () => <HomePage /> }
-      />
-    </Switch>
+    <ThemeProvider theme={ darkTheme }>
+      <AlertTogle
+        severity={ severity }
+        switchValue={ [open, setOpenAlert] }
+      >
+        {alertGlobal.value}
+      </AlertTogle>
+      <Switch>
+        <Route
+          exact
+          path="/login"
+          render={ () => <Login /> }
+        />
+        <Route
+          exact
+          path="/"
+          render={ () => <HomePage /> }
+        />
+      </Switch>
+    </ThemeProvider>
   );
 }
 
